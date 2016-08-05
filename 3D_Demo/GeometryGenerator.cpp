911b29f299cc94fbe371b9795424ca5f3ea472fa
Vampire <846827137@qq.com>
2016-08-05 14:57:24 +0800

    Add Geometry Generator

---
+++ 3D_Demo/GeometryGenerator.cpp
@@ -0,0 +1,193 @@
+#include "GeometryGenerator.h"
+
+
+/****** Public Function ***************************************/
+void GeometryGenerator::CreateBox(float width, float height, float depth, MeshData &meshData)
+{
+	meshData.Vertices.clear();
+	meshData.Indices.clear();
+#pragma region Create the Vertices
+	Vertex v[24];
+
+	float w = 0.5f*width;
+	float h = 0.5f*height;
+	float d = 0.5f*depth;
+
+	// Fill in the front face vertex data.
+	v[0] = Vertex(-w, -h, -d, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[1] = Vertex(-w, +h, -d, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[2] = Vertex(+w, +h, -d, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	v[3] = Vertex(+w, -h, -d, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+
+	// Fill in the back face vertex data.
+	v[4] = Vertex(-w, -h, +d, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[5] = Vertex(+w, -h, +d, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[6] = Vertex(+w, +h, +d, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[7] = Vertex(-w, +h, +d, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	// Fill in the top face vertex data.
+	v[8] = Vertex(-w, +h, -d, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[9] = Vertex(-w, +h, +d, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[10] = Vertex(+w, +h, +d, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	v[11] = Vertex(+w, +h, -d, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+
+	// Fill in the bottom face vertex data.
+	v[12] = Vertex(-w, -h, -d, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+	v[13] = Vertex(+w, -h, -d, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+	v[14] = Vertex(+w, -h, +d, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	v[15] = Vertex(-w, -h, +d, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	// Fill in the left face vertex data.
+	v[16] = Vertex(-w, -h, +d, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[17] = Vertex(-w, +h, +d, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[18] = Vertex(-w, +h, -d, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[19] = Vertex(-w, -h, -d, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+
+	// Fill in the right face vertex data.
+	v[20] = Vertex(+w, -h, -d, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+	v[21] = Vertex(+w, +h, -d, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+	v[22] = Vertex(+w, +h, +d, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+	v[23] = Vertex(+w, -h, +d, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+
+	meshData.Vertices.assign(&v[0], &v[24]);
+#pragma endregion
+#pragma region Create the Indices
+	UINT i[36];
+
+	// Fill in the front face index data
+	i[0] = 0; i[1] = 1; i[2] = 2;
+	i[3] = 0; i[4] = 2; i[5] = 3;
+
+	// Fill in the back face index data
+	i[6] = 4; i[7] = 5; i[8] = 6;
+	i[9] = 4; i[10] = 6; i[11] = 7;
+
+	// Fill in the top face index data
+	i[12] = 8; i[13] = 9; i[14] = 10;
+	i[15] = 8; i[16] = 10; i[17] = 11;
+
+	// Fill in the bottom face index data
+	i[18] = 12; i[19] = 13; i[20] = 14;
+	i[21] = 12; i[22] = 14; i[23] = 15;
+
+	// Fill in the left face index data
+	i[24] = 16; i[25] = 17; i[26] = 18;
+	i[27] = 16; i[28] = 18; i[29] = 19;
+
+	// Fill in the right face index data
+	i[30] = 20; i[31] = 21; i[32] = 22;
+	i[33] = 20; i[34] = 22; i[35] = 23;
+
+	meshData.Indices.assign(&i[0], &i[36]);
+#pragma endregion
+}
+
+void GeometryGenerator::CreateSphere(float radius, UINT sliceCount, UINT stackCount, MeshData &meshData)
+{
+	meshData.Vertices.clear();
+	meshData.Indices.clear();
+#pragma region Create the Vertices
+	//Create Poles Vertices
+	Vertex topVertex(0.0f, +radius, 0.0f, 0.0f, +1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+	Vertex bottomVertex(0.0f, -radius, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+
+	meshData.Vertices.push_back(topVertex);
+
+	float phiStep = XM_PI / stackCount;
+	float thetaStep = XM_2PI / sliceCount;
+	for ( UINT i = 1; i <= stackCount - 1; ++i )
+	{
+		float phi = i*phiStep;
+		for ( UINT j = 0; j <= sliceCount; ++j )
+		{
+			float theta = j*thetaStep;
+			Vertex v;
+
+			// spherical to cartesian
+			v.Position.x = radius*sinf(phi)*cosf(theta);
+			v.Position.y = radius*cosf(phi);
+			v.Position.z = radius*sinf(phi)*sinf(theta);
+
+			// Partial derivative of P with respect to theta
+			v.TangentU.x = -radius*sinf(phi)*sinf(theta);
+			v.TangentU.y = 0.0f;
+			v.TangentU.z = +radius*sinf(phi)*cosf(theta);
+
+			XMVECTOR T = XMLoadFloat3(&v.TangentU);
+			XMStoreFloat3(&v.TangentU, XMVector3Normalize(T));
+
+			XMVECTOR p = XMLoadFloat3(&v.Position);
+			XMStoreFloat3(&v.Normal, XMVector3Normalize(p));
+
+			v.TexC.x = theta / XM_2PI;
+			v.TexC.y = phi / XM_PI;
+
+			meshData.Vertices.push_back(v);
+		}
+	}
+
+	meshData.Vertices.push_back(bottomVertex);
+#pragma endregion
+#pragma region Create the Indices
+	for ( UINT i = 1; i <= sliceCount; ++i )
+	{
+		meshData.Indices.push_back(0);
+		meshData.Indices.push_back(i + 1);
+		meshData.Indices.push_back(i);
+	}
+
+	UINT baseIndex = 1;
+	UINT ringVertexCount = sliceCount + 1;
+	for ( UINT i = 0; i < stackCount - 2; ++i )
+	{
+		for ( UINT j = 0; j < sliceCount; ++j )
+		{
+			meshData.Indices.push_back(baseIndex + i*ringVertexCount + j);
+			meshData.Indices.push_back(baseIndex + i*ringVertexCount + j + 1);
+			meshData.Indices.push_back(baseIndex + ( i + 1 )*ringVertexCount + j);
+
+			meshData.Indices.push_back(baseIndex + ( i + 1 )*ringVertexCount + j);
+			meshData.Indices.push_back(baseIndex + i*ringVertexCount + j + 1);
+			meshData.Indices.push_back(baseIndex + ( i + 1 )*ringVertexCount + j + 1);
+		}
+	}
+
+	UINT southPoleIndex = (UINT)meshData.Vertices.size() - 1;
+	baseIndex = southPoleIndex - ringVertexCount;
+	for ( UINT i = 0; i < sliceCount; ++i )
+	{
+		meshData.Indices.push_back(southPoleIndex);
+		meshData.Indices.push_back(baseIndex + i);
+		meshData.Indices.push_back(baseIndex + i + 1);
+	}
+#pragma endregion
+}
+
+void GeometryGenerator::CreateGeosphere(float radius, UINT numSubdivisions, MeshData &meshData)
+{
+}
+
+void GeometryGenerator::CreateCylinder(float bottomRadius, float topRadius, float height, UINT sliceCount, UINT stackCount, MeshData &meshData)
+{
+}
+
+void GeometryGenerator::CreateGrid(float width, float depth, UINT rowCount, UINT columnCount, MeshData &meshData)
+{
+}
+
+void GeometryGenerator::CreateFullscreenQuad(MeshData &meshData)
+{
+}
+
+/****** Protected Function ***************************************/
+void GeometryGenerator::Subdivide(MeshData &meshData)
+{
+}
+
+void GeometryGenerator::BuildCylinderTopCap(float bottomRadius, float topRadius, float height, UINT sliceCount, UINT stackCount, MeshData &meshData)
+{
+}
+
+void GeometryGenerator::BuildCylinderBottomCap(float bottomRadius, float topRadius, float height, UINT sliceCount, UINT stackCount, MeshData &meshData)
+{
+}
